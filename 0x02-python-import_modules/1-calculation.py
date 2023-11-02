9fba439fa306176ea019c1c3c9e675b49b1fa7a5
PrincewillDev <princewillfidelis1@gmail.com>
2023-11-02 15:23:43 -0700

    Fixed error

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add
-    from calculator_1 import sub
-    from calculator_1 import mul
-    from calculator_1 import div
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    addi = add(a, b)
-    subt = sub(a, b)
-    mult = mul(a, b)
-    divi = div(a, b)
-
-    print(addi)
-    print(subt)
-    print(mult)
-    print(divi)
+    print("{} + {} = {}" .format(a, b, add(a, b)))
+    print("{} - {} = {}" .format(a, b, sub(a, b)))
+    print("{} * {} = {}" .format(a, b, mul(a, b)))
+    print("{} / {} = {}" .format(a, b, div(a, b)))
