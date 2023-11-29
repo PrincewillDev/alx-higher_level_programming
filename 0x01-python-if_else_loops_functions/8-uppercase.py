d55ad0fa8fb1a8a478bd257d14a923e2ab0e2144
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-11-30 00:10:00 +0100

    fix: fix a the code to fit checker guideline for task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
             char = chr(ord(char) - 32)
-            print('{}'.format(char), end="")
+            result += char
         else:
-            print('{}'.format(char), end="")
-    print()
+            result += char
+    print(result)
