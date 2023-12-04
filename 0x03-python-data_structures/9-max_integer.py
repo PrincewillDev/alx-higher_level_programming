e738039a039c410221a858965945513f044a3bb4
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-12-04 23:46:21 +0100

    fix: fix a bug to fit checker guideline (Task 9)

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     maxNum = my_list[0]
     if len(my_list) == 0:
         return (None)
-    for i in my_list:
+    for i in range(1, len(my_list)):
         if my_list[i] > maxNum:
             maxNum = my_list[i]
     return (maxNum)
