059ea98cc2e792f8fc2c4cc04e6a81674831c81e
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-12-04 21:53:43 +0100

    feat: add new task (Task 8)

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return (None)
+        return None
     else:
         newTuple = (len(sentence), sentence[0])
         return (newTuple)
