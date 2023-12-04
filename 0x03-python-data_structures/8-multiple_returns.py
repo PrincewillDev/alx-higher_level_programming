d13021e5fa93c0fa3a5967d79484ae18b48a6fd5
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-12-04 21:56:37 +0100

    fix: fix a bug to fit checker guideline (Task 8)

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
+        newTuple = (len(sentence), None) 
     else:
         newTuple = (len(sentence), sentence[0])
-        return (newTuple)
+    return (newTuple)
