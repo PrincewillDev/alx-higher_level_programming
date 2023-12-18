dc6a1e780639d49d3873f046ae49ec10e6c036d3
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-12-18 22:58:26 +0100

    feat: this function divide 2 int and print result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+        if b == 0:
+            raise(ZeroDivisionError)
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(result))
