0504b4b5b5ed1a62320061654b8f04f36eeb4408
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-01-13 14:29:40 +0100

    feat: add a module and class method

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
 
+"""
+This module defines an empty square class
+
+Class:
+    Square: This class is a representation of a square shape, with no attributes and method.
+
+Attributes: None
+
+Method: None
+
+"""
+
 class Square:
-    """This is an empty class that defines a square"""
+    """
+    This class represent a square but it doesn't have any attribute or method, which will be built as the program advances.
+    
+    Attributes: None
+
+    Method: None
+
+
+    """
     pass
