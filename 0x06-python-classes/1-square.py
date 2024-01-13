96e39844f8d9b116c5c219950bab0ac4ebfaa684
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-01-13 16:12:42 +0100

    feat: add documentation using docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,40 @@
 #!/usr/bin/python3
 
+"""
+This module defines a class (Square)
+
+Class:
+    Square: Represent a square
+
+Attribute:
+   __size: A private instance attribute (The size of square).
+
+Method:
+    __init__: Initializes a square with a given size.
+
+"""
+
+
 class Square:
+
+    """
+    Represents a square with a private instance attribute of __size.
+
+    Attributes:
+        __size (int): The size of square (a private instance attribute)
+
+    Method:
+        __init__(size): Initializes the size of square
+
+    """
+
     def __init__(self, size):
+
+        """
+        Initializes the size of the square
+
+        parameters:
+            size (int): The size of the square.
+
+        """
         self.__size = size
