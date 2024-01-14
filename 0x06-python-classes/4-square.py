b21e10371f709ffec21cff772e5d7d252562554e
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-01-14 12:55:46 +0100

    feat: add a getter and setter property to handle the attribute securely and docustrings

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+"""
+This module define a class (Square)
+
+Class:
+    Square: represents a square
+
+Attributes:
+    __size: A private instance attribute (the size of a square)
+
+Method:
+    __init__(size): Initializes the size of a square
+
+    area: Calculates the area of a square
+
+Properties:
+    size: This property is use to get the attribute size (getter)
+    size: This property with @size.setter set the value of size (setter)
+
+"""
+
+
+class Square:
+
+    """
+   This class  represents a square
+
+    Attributes:
+        __size: A private instance attribute (the size of a square)
+
+    Method:
+        __init__(size): Initializes the size of a square
+
+        area: Calculates the area of a square
+
+    Properties:
+        size: This property get the __size private instance attribute (getter)
+        size: This property with @size.setter set the value of size (setter)
+
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        """
+        Initializes the size of a square
+
+         parameter:
+            size: The size of a square
+
+        """
+
+    @property
+    def size(self):
+
+        """
+        This property is use retrive the __size private instance attribute
+        (Getter)
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        """
+        This property set the value of size (setter)
+
+          parameter:
+            value: The value of size
+
+        checks:
+            raise TypeError if value is not an integer
+
+            raise ValueError if value is less than 0
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+
+        """
+        Calculates the area of a square
+
+        """
+        value = self.size * self.size
+        return value
