d86d9c0639e93f0c2f7fe904993adc7f09ee47f4
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-01-27 01:19:29 +0100

    feat: add function that add two integers and wrote test case for the function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Adds two integers
+Parameters: a, b
+Returns: int: The sum of a and b
+"""
+
+def add_integer(a, b=98):
+    """ Adds two intgers 
+        Returns: int: The sum of a and b
+    """
+    if not isinstance(a, (int, float)):
+        """ raise TypeError if a is not an integer """
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        """ raise TypeError if b is not an integer """
+        raise TypeError("b must be an integer")
+
+    result = int(a + b)
+    return result
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,2 @@
+## Python - Test-driven development
+### 0-add_integer.py: This function adds 2 integers.
