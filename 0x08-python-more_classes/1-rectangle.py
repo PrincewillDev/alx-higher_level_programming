df824fd7b6b80a8317c6e6bc7ba6ad53d2150211
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-01-29 20:02:37 +0100

    Fix: fixed a syntax error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -71,9 +71,9 @@ class Rectangle:
         """ Setter property to set the value of height"""
 
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
 
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
 
         self._height = value
