9f188a2127b0084f159193143a49e9e8a4142ea5
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-01-29 18:20:45 +0100

    feat: add empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module creates an empty class that defines
+a rectangle
+
+Class:
+    Rectangle: Defines a rectangle
+
+Attribute:
+    None
+
+Method:
+    None
+"""
+
+class Rectangle:
+    """ This is an empty class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+## Python - More Classes and Objects
+### 0-rectangle.py: This is an empty class that defines a rectangle
