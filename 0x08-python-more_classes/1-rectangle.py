f818e2276aa9fe4f0abba65461916f73cfc43d92
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-01-29 20:09:26 +0100

    Fix: fixed a syntax error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -56,7 +56,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
 
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
 
         self._width = value
@@ -73,7 +73,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("height must be an integer")
 
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
 
         self._height = value
