8f1fe8921db27f8e5e5010e264a69e02f6cdf955
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-02-04 19:27:59 +0100

    feat: add an __repr__ method to use and print a string representation of the Rectangle class

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python3
+
+"""
+This module creates an class that calculate the
+area and perimeter
+
+Class:
+    Rectangle: Defines a rectangle
+
+Attribute:
+    width: The width of a rectangle
+    Height: The height of a rectangle
+
+Method:
+    __init__(width, height): width and height of a rectangle
+
+    area: calculate the area of a rectangle
+
+    perimeter: calculate the perimeter of a rectangle
+
+    __str__: Print rectangle with the # character
+
+    __repr__: Return str representation of rectangle
+    to be able to recreate a new instance with eval()
+
+"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle by its width and heights
+
+    Attribute:
+    width: The width of a rectangle
+    Height: The height of a rectangle
+
+    Method:
+    __init__(width, height): width and height of a rectangle
+
+    area: calculate the area of a rectangle
+
+    perimeter: calculate the perimeter of a rectangle
+
+    __str__: Print rectangle with the # character
+
+    __repr__: Return str representation of rectangle
+    to be able to recreate a new instance with eval()
+
+    Raise:
+        TypeError:
+                i) If width is not Integer
+                ii) If height is not integer
+        ValueError:
+                i) If width is less than 0
+                ii) If height is less than 0
+
+    """
+    def __init__(self, width=0, height=0):
+        """ Initializes the width and height
+        with optional size
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ Getter property for width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter property to set the value of width"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter property for height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter property to set the value of height"""
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        value = self.width * self.height
+        return value
+
+    def perimeter(self):
+        """ Calculate the perimeter """
+        if self.width == 0 or self.height == 0:
+            value = 0
+        else:
+            value = 2 * (self.width + self.height)
+        return value
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            result = ""
+            for i in range(self.height):
+                result += '#' * self.width + '\n'
+            return result[:-1]
+
+    def __repr__(self):
+        return "Rectangle(width={}, height={})".format(self.width, self.height)
