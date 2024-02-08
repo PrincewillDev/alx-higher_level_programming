c483e9b851bef2d1e72fc160766e8d9029c0249d
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-02-08 14:50:50 +0100

    feat: add a function to check for attributes and method in an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This module checks and returns attributes
+and methods of an object
+
+"""
+
+
+def lookup(obj):
+    """
+    This function takes an object and returns the
+    attribute and method of the object
+
+    Parameters:
+    obj: This is the object to be checked and returns
+    its available attributes and methods.
+
+    Returns:
+    Attributes and Methods of obj
+
+    """
+    obj_list = []
+    obj_list.append(dir(obj))
+
+    return obj_list
