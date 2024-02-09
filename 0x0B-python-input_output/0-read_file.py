58911ac12f0aae3d045c05bb66668de72041b732
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-02-10 00:37:07 +0100

    feat: add a function to read text fileand print its content to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This reads a text file and print
+the text file data to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    This function takes a text file as input,
+    read the text data and print to stdout.
+
+    Parameter:
+    filename: the text filename to be read and print
+    to stdout
+    """
+    # open the text file and read
+    with open(filename, 'r', encoding="UTF-8") as text_file:
+        read_file = text_file.read()
+
+    # Print its content to stdout
+    print(read_file)
