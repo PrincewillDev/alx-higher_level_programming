767b98d13741ac565ffef2b98e62ddda57d0a9de
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-03-29 16:00:18 +0100

    This script lists the number of records with the same score in the second_table

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
 -- This script lists the number of records with the same score in the second_table
 
-SELECT score, COUNT(*)
+SELECT score, COUNT(score) as number
     FROM second_table
     GROUP BY score
+    ORDER BY number DESC
