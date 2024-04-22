3bb0da8ba9ae9e231499ed2af9cb4220251737fd
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-04-22 22:31:53 +0100

    Created a simple commandline javascript script

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const args = process.argv;
 const length = args.length;
 if (length < 3) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (length === 3) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
