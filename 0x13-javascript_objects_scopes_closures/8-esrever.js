6c31c25b10d670c95bf53948c4e351d577553aa2
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-04-25 00:28:42 +0100

    Created a function that returns a reversed array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    reversedList = [];
-    const arryLength = list.length;
-    for (let i = arryLength - 1; i >= 0; i--){
+  const reversedList = [];
+  const arryLength = list.length;
+  for (let i = arryLength - 1; i >= 0; i--) {
     reversedList.push(list[i]);
   }
   return reversedList;
