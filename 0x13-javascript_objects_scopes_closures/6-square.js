2e76562a282c4503d519600c091ca96bef2979f5
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-04-24 23:41:49 +0100

    Created a Square subclass that inherits from another Square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const ImportSquare = require('./5-square.js');
+
+class Square extends ImportSquare {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
+}
+module.exports = Square;
