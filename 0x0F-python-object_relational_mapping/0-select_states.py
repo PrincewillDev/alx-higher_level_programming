53d5df022a3d98572d997c8c87d6feffbc780ee5
PrincewillDev <princewillfidelis1@gmail.com>
2024-04-29 17:03:12 +0100

    Fixed space errors to fit pycodestyle requirement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     dbname = sys.argv[3]
 
-    db = MySQLdb.connect(host = "localhost", port = 3306, user = username, passwd = password, db = dbname)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=dbname)
 
     query = "SELECT * FROM states ORDER BY states.id ASC"
 
