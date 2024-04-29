0cb7b37969dcc4442941326a9bd91c1fe0c4ba67
PrincewillDev <princewillfidelis1@gmail.com>
2024-04-29 16:50:18 +0100

    Created script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,27 @@
-"""
-This script interacts with the mysql database by listing all states
-"""
 #!/usr/bin/python3
+"""
+This script contains a function that lists all states from a database..
+Note: database name is passed as argument to the script.
+"""
+if __name__ == "__main__":
 
-import sys
-import MySQLdb
+    import sys
+    import MySQLdb
 
-# Assigning the arguments with their index
-username = sys.argv[1]
-password = sys.argv[2]
-dbname = sys.argv[3]
+    # Assigning the arguments with their index
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
 
-db = MySQLdb.connect(host = "localhost", port = 3306, user = username, passwd = password, db = dbname)
+    db = MySQLdb.connect(host = "localhost", port = 3306, user = username, passwd = password, db = dbname)
 
-query = "SELECT * FROM states ORDER BY states.id ASC"
+    query = "SELECT * FROM states ORDER BY states.id ASC"
 
-cursor = db.cursor()
-cursor.execute(query)
-result = cursor.fetchall()
-for r in result:
-    print(r)
+    cursor = db.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for r in result:
+        print(r)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
