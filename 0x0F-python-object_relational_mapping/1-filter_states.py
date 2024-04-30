3fda41e34605b24a066b2b4bfd6f8d5863dd7a15
PrincewillDev <princewillfidelis1@gmail.com>
2024-04-30 01:29:24 +0100

    CrFixed venv errors in ORM repo

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This script contains a function that lists all states from a database starting with learner N..
+Note: database name is passed as argument to the script.
+"""
+if __name__ == "__main__":
+
+    import sys
+    import MySQLdb
+
+    # Assigning the arguments with their index
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbname
+    )
+
+    query = "SELECT * FROM states WHERE name LIKE 'P%' ORDER BY states.id ASC"
+
+    cursor = db.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for r in result:
+        print(r)
+
+    cursor.close()
+    db.close()
