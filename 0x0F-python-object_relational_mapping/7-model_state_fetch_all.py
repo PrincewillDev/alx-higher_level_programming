0b364b54e9df6b5232240876e966be59ba1f9af2
PrincewillDev <princewillfidelis1@gmail.com>
2024-06-23 20:51:33 +0100

    Created a script that lists all State objects from the database hbtn_0e_6_usa using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,40 @@
+"""
+This script lists all State objects from the database hbtn_0e_6_usa
+using sqlalchemy
+"""
+
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+# Assigning the arguments with their index
+try:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+except IndexError:
+    print(f"Usage: {sys.argv[0]} <username> <password> <databasename>")
+    sys.exit(1)
+
+# Create engine
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost/{}'
+    .format(username, password, dbname), pool_pre_ping=True
+    )
+# Base.metadata.create_all(engine)
+
+# Create Session Class
+Session = sessionmaker(bind=engine)
+session = Session()
+
+try:
+    # Use the session to query the database
+    rows = session.query(State).order_by(asc(State.id)).all()
+
+    for item in rows:
+        print(f"{item.id}: {item.name}")
+finally:
+    # Close the session
+    session.close()
