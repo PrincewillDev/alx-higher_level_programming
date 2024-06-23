aeecf9b00858aaaac0998eb3123a09460ad96cab
PrincewillDev <princewillfidelis1@gmail.com>
2024-06-23 18:50:48 +0100

    Created a script that contains the class definition of a State and an instance Base = declarative_base():

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script contains the class definition of
+a State and an instance Base = declarative_base():
+"""
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create a base class for our models
+Base = declarative_base()
+
+# Define our User model
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
