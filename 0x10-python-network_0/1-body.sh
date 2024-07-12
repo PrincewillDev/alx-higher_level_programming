07247a6d0a2002334aeab4ec63811622a4c7aea3
PrincewillDev <princewillfidelis1@gmail.com>
2024-07-12 15:16:02 +0100

    new

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Takes a URL, sends a GET request and displays the body of response
 curl -s -L "${1}"
+
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Tcript sends a POST request to a URL received and displays response body
-curl -sd "email=test@gmail.com&&subject=I will always be here for PLD" -X POST "$1" 
+curl -sd "email=test@gmail.com&&subject=I will always be here for PLD" -X POST "$1"
